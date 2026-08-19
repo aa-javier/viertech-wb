29cdd20e6dbc052adcf1460f1a6023ee276dd632
AA Javier <arkhaniphone@gmail.com>
2026-08-19 19:31:03 +0700

    Add files via upload

---
+++ example/sticker-pack.js
@@ -0,0 +1,11 @@
+const { StickerPack } = require('@viertech/wb')
+
+// await StickerPack.send(client, chat, {
+//   name: 'VierTech Pack',
+//   publisher: 'VierTech',
+//   cover: coverBuffer,
+//   stickers: [
+//     { buffer: firstSticker, emojis: ['🔥'] },
+//     { buffer: secondSticker, emojis: ['😂'] }
+//   ]
+// })
