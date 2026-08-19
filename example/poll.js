b4376126dae61de3a57a41643e42685d54074b22
AA Javier <arkhaniphone@gmail.com>
2026-08-19 19:30:33 +0700

    Create poll.js with Poll functionality template
    Add initial poll.js file with commented-out code for Poll functionality.

---
+++ example/poll.js
@@ -0,0 +1,15 @@
+const { Poll } = require('@viertech/wb')
+
+// const sent = await Poll.send(
+//   client,
+//   chat,
+//   'Pilih target',
+//   ['PLAYER 1', 'PLAYER 2'],
+//   { selectableCount: 1 }
+// )
+
+// const result = await Poll.parseUpdate(message, sent, {
+//   client,
+//   participants,
+//   pollUpdates: []
+// })
